1c925bb25ab250816a229523946dfd947baaa184
Jason <jyoung12387@gmail.com>
2022-07-12 21:33:26 -0400

    SSR with NextJS

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,32 @@
 import { useEffect, useState } from "react";
 
-export default function Home() {
-	// set state initially to an empty array
-	// that way we can use conditional rendering to check
-	// if the array has a length of 0
-	const [todos, setTodos] = useState([]);
+export async function getServerSideProps() {
+	const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+	const data = await res.json();
 
-	// standard useEffect to fetch data from api
-	// put it it's own funciton so we can use async await syntax
-	// make sure to call the funciton
-	useEffect(() => {
-		const fetchTodos = async () => {
-			const res = await fetch("https://jsonplaceholder.typicode.com/todos");
-			const data = await res.json();
-			setTodos(data);
-		};
-		fetchTodos();
-	}, []);
+	return {
+		props: {
+			todos: data,
+		},
+	};
+}
+
+export default function Home({ todos }) {
+	// // set state initially to an empty array
+	// // that way we can use conditional rendering to check
+	// // if the array has a length of 0
+	// const [todos, setTodos] = useState([]);
+
+	// // standard useEffect to fetch data from api
+	// // put it it's own funciton so we can use async await syntax
+	// // make sure to call the funciton
+	// useEffect(() => {
+	// 	const fetchTodos = async () => {
+
+	// 		setTodos(data);
+	// 	};
+	// 	fetchTodos();
+	// }, []);
 
 	// This will render the page with a quick loading message
 	// before the data is fetched.
@@ -25,10 +35,10 @@ export default function Home() {
 			{/* conditional rendering. checking to see if the array.length === 0
       if so, page will render without data */}
 
-			{todos.length === 0 ? (
+			{todos?.length === 0 ? (
 				<div>Loading...</div>
 			) : (
-				todos.map((todo) => (
+				todos?.map((todo) => (
 					<div key={todo.id}>
 						<p>
 							{todo.id}: {todo.title}
